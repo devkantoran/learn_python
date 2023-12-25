d81c78a45a184aab35de61c71f5c5a9c201f626a
DevKantoran <devkantoran@outlook.com>
2023-12-25 19:36:04 +0700

    Chapter 7 done

---
+++ 07 calcRange.py
@@ -0,0 +1,8 @@
+# Calculate the total of numbers up to a number entered by the user
+usersNumber = input("Please enter an integer: ")
+usersNumber = int(usersNumber)
+highEndOfRange = usersNumber + 1
+total = 0
+for number in range(1, highEndOfRange):
+    total = total + number
+print("The total numbers from 1 to", usersNumber, "is", total)
