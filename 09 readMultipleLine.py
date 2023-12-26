c77e8c62e1c9e0c4d60ee60c385a97fb0d1d073e
DevKantoran <devkantoran@outlook.com>
2023-12-26 13:52:04 +0700

    Chapter 9 completed

---
+++ 09 readMultipleLine.py
@@ -0,0 +1,13 @@
+from FileReadWrite import *
+
+# Read in multiple lines of text
+DATA_FILE_PATH = "MultiLineData.txt"
+myFileHandle = open(DATA_FILE_PATH)
+data1 = readALine(myFileHandle)
+print(data1)
+data2 = readALine(myFileHandle)
+print(data2)
+data3 = readALine(myFileHandle)
+print(data3)
+# Could add code to split data3 into several different pieces of data
+closeFile(myFileHandle)
